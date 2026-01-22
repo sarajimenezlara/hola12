0bebb5255daf7e188252f5ed8ec484f952669238
sarajimenezlara <sarajimlar207@gmail.com>
2026-01-22 12:35:18 +0100

    add  holareact

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App.tsx'
 import List from './components/List.tsx'
 import 'bootstrap/dist/css/bootstrap.css';
+import HolaReact from "./components/HolaReact.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <App />
     <List />
+    <HolaReact />
   </StrictMode>,
 )
